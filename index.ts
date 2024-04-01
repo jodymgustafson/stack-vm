85ae0f4a554c57723406d8b73f376e8a7dea1472
JM Gustafson <jodymgustafson@hotmail.com>
2024-04-01 13:19:51 -0400

    loader improvement

--- index.ts
+++ index.ts
@@ -14,19 +14,13 @@ import { StackVmSystemConsoleLib } from "./src/internal/stack-vm-console";
 import { instructionLogger } from "./src/internal/instruction-logger";
 import { StackVmConfig } from "./src/stackvm-types";
 
+const loadedFns = new StackVmLoader().loadSync(argv[2]);
+
 const config: StackVmConfig = {
-    functions: { ...StackVmSystemMathLib, ...StackVmSystemStringLib, ...StackVmSystemConsoleLib },
+    functions: { ...loadedFns, ...StackVmSystemMathLib, ...StackVmSystemStringLib, ...StackVmSystemConsoleLib },
     // stackLogger: (...s) => console.log(...s),
     // instructionLogger: instructionLogger,
 };
-
-const assembler = new StackVmAssembler();
-const pkg = new StackVmLoader().loadSync(argv[2]);
-// console.log(JSON.stringify(pkg, null, 2));
-for (const fn of pkg.stackvm.package.functions) {
-    const asm = assembler.assemble(fn.definition);
-    config.functions[fn.name] = asm;
-}
 
 const vm = new StackVM(config);
 const result = vm.run();
--- src/stack-vm-loader.ts
+++ src/stack-vm-loader.ts
@@ -1,6 +1,9 @@
 import * as fs from "fs";
 import YAML from 'yaml';
 import { StackVM } from "./stack-vm";
+import path from "path";
+import { StackVmAssembler } from "./stack-vm-assembler";
+import { StackVmFunctionsMap } from "./stackvm-types";
 
 export class StackVmLoaderError extends Error {
 }
@@ -22,7 +25,7 @@ export type StackVmFile = {
     stackvm: {
         version: string;
         package?: StackVmPackage
-        depends?: string[];
+        import?: string[];
     }
 };
 
@@ -30,26 +33,31 @@ export type StackVmFile = {
  * Loads a program from a YAML file
  */
 export class StackVmLoader {
-    // constructor(readonly vm: StackVM) {}
+    constructor(readonly assembler = new StackVmAssembler()) {}
 
     /**
-     * Loads the program from a YAML file
+     * Loads the program from a YAML file and returns a map of assembled functions ready to be used by the VM.
      * @returns The contents of the file as a StackVmFile object
      */
-    loadSync(filePath: string): StackVmFile {
+    loadSync(filePath: string, functions: StackVmFunctionsMap = {}): StackVmFunctionsMap {
         const s = fs.readFileSync(filePath, "utf-8");
         const content = YAML.parse(s) as StackVmFile;
         if (!content.stackvm) throw new StackVmLoaderError("File does not conform to StackVM format");
 
-        if (content.stackvm.depends) {
-            for (const dep of content.stackvm.depends) {
-
+        if (content.stackvm.import) {
+            const base = path.dirname(filePath);
+            for (const dep of content.stackvm.import) {
+                this.loadSync(path.join(base, dep), functions);
             }
         }
 
         if (content.stackvm.package) {
+            for (const fn of content.stackvm.package.functions) {
+                const asm = this.assembler.assemble(fn.definition);
+                functions[fn.name] = asm;
+            }
         }
 
-        return content;
+        return functions;
     } 
 }
